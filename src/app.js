684229d13103e8a27cd6707306c0399bd0b8acf5
MatiasRomeo <m.romeov@hotmail.com>
2023-07-17 02:04:58 -0400

    APIFAINAL

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import cors from 'cors'
+import express from 'express'
+import { startConnection } from './config/database.js'
+import environment from './config/environment.js'
+import authRoutes from './resources/auth/routes/auth.routes.js'
+import productsRouter from './resources/products/routes/products.routes.js'
+import usersRouter from './resources/users/routes/users.routes.js'
+
+const app = express()
+
+startConnection()
+
+app.use( express.json() )
+app.use( cors() )
+
+app.get( '/', function ( req, res ) {
+  return res.status( 200 ).json( { msg: "Bienvenido" } )
+} )
+
+app.use( usersRouter )
+app.use( productsRouter )
+app.use( authRoutes )
+
+const { PORT } = environment
+app.listen( PORT, () => {
+  console.log( `Aplicación iniciada en puerto ${ PORT }` )
+} )
